721793b1d5a251451a49d5e89f3b3729099477a8
asasmith <asantwa.smith@gmail.com>
2024-10-22 20:03:34 -0400

    File uploads

---
+++ index.js
@@ -0,0 +1,12 @@
+function startGame(){
+
+    var diceOne = random(6, 1);
+    var diceTwo = random(6, 1);
+
+    changeImage('img1', diceOne);
+    changeImage('img2', diceTwo);
+
+// I need to create an if statement for the dice to change (if,elseif & else)
+
+
+}
